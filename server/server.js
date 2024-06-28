01fad42615b15535e7e8944f362998ac1e3ccf16
Christopher-Petersen <crisspetersen@gmail.com>
2024-06-28 09:45:04 -0600

    server.js code mostly complete

---
+++ server/server.js
@@ -0,0 +1,63 @@
+//Import packages and files
+import express from "express"
+import cors from "cors"
+
+
+let mockData = [
+    {
+        "id": 1
+        "task": 'Buy Groceries'
+    },
+    {
+        "id": 2
+        "task": 'Walk the dog'
+    },
+    {
+        "id": 3
+        "task": 'Save the cheerleader' //Heroes reference is baller!
+    },
+    {
+        "id": 4
+        "task": 'Save the world'
+    },
+
+    let globalId = 5
+]
+
+//Setup express instance
+const app = express()
+
+//Setup middleware
+app.use(express.json())
+app.use(cors())
+appl.use(express.static('src'))
+
+//ENDPOINTS GO HERE
+app.get(`/toDoList`, (req, res) => {
+    res.send(mockData)
+})
+
+app.post(`newToDo`, (req, res) => {
+    const newListItem = {
+        id: globalId,
+        task: req.body.task,
+    }
+    mockData.push(newListItem)
+    globalId++
+
+    res.send(mockData)
+})
+
+app.delete(`toDoList/:id`, (req, res) => {
+    console.log("PARAMS", req.params)
+
+    const idToDelete = +request.params.id
+    
+    mockData = mockData.filter((el) => el.id != idToDelete)
+
+    res.send(mockData)
+})
+
+//Open sever using app.listen
+
+app.listen(2112, () => console.log('Sever running at http://localhost:2112'))
